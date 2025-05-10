00306b859cf04b822cbdde061ba314471bcc4aa2
Nadila Kulanatha <dithmal@protonmail.com>
2025-05-10 23:02:41 +0530

    min: SEO updates

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Nadila';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = "Hey I'm Nadila. This is where I document things that are interesting to me";
