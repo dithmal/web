5beab537385830a4c5aa2e0b43960f52902df0d2
Nadila Kulanatha <dithmal@protonmail.com>
2025-05-11 22:22:59 +0530

    fix: posthog error

--- src/configs.ts
+++
@@ -1,4 +0,0 @@
-export const configs = { // replace the following with your own values.
-    posthog_api_key: "phc_D8edDJ0GnrfVMFnSW2mbbkUcOgjLywTUYQqAC8v4t3m",
-    posthog_host: "https://spider.dithmal.lk"
-}
