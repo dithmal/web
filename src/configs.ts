d4fc88abd4d5ad17262e0725bbede08f170e9c57
Nadila Kulanatha <dithmal@protonmail.com>
2025-05-11 21:52:22 +0530

    feat: add posthog

---
+++ src/configs.ts
@@ -0,0 +1,4 @@
+export const configs = { // replace the following with your own values.
+    posthog_api_key: "phc_D8edDJ0GnrfVMFnSW2mbbkUcOgjLywTUYQqAC8v4t3m",
+    posthog_host: "https://spider.dithmal.lk"
+}
